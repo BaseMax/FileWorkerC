c77397ec00829b8acdb47b4fb35175f629d7e86f
Max Base <maxbasecode@gmail.com>
2023-02-04 23:38:07 +0000

    update

--- FileWorker.c
+++ FileWorker.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void invalid()
 {
@@ -63,31 +64,105 @@ void parse(int argc, char** argv)
             printf("insertstr: invalid arguments try `--file <name> --str <value> --pos <line>:<index>`\n");
         }
     } else if (strcmp(command, "cat") == 0) {
-        cat(argc, argv);
+        // cat(argc, argv);
     // find –str ”an expression with \* but not wildcard” –file a.txt
     // find –str fire –file a.txt -count
     // find –str fire –file a.txt -at 3
     // find –str ”salam khubi*” –file a.txt -byword
     // find –str ”difficult project” –file a.txt -all -byword
     } else if (strcmp(command, "find") == 0) {
-        cat(argc, argv);
+        // cat(argc, argv);
     // replace –str1 <str> –str2 <str> –file <file name> [-at <num> | -all]
     } else if (strcmp(command, "replace") == 0) {
-        replace(argc, argv);
+        // replace(argc, argv);
     } else if (strcmp(command, "compare") == 0) {
-        compare(argc, argv);
+        // compare(argc, argv);
     } else {
         invalid();
     }
 }
 
+char* skip_string(char* line, int length, int skip)
+{
+    int i = 0;
+    char* result = malloc(sizeof(char) * (length - skip));
+
+    for (int j = skip + 1; j < length; j++) result[i++] = line[j];
+
+    result[i] = '\0';
+    return result;
+}
+
+char **split_string(char *input, char sep, int *count)
+{
+    int i = 0;
+    char **result = malloc(sizeof(char*) * 512);
+    char *token = strtok(input, &sep);
+
+    while (token != NULL) {
+        result[i++] = token;
+        token = strtok(NULL, &sep);
+    }
+
+    result[i] = NULL;
+    *count = i;
+    return result;
+}
+
 int main(int argc, char** argv)
 {
-    if (argc == 1) {
-        help();
-    } else if (argc >= 2) {
-        argv++;
-        parse(argc - 1, argv);
+    int flag = 1;
+    while (flag == 1) {
+        printf("FileWorker> ");
+        char* input = malloc(512);
+        fgets(input, 512, stdin);
+        int len = strlen(input);
+        if (input[len - 1] == '\n') {
+            input[len - 1] = '\0';
+        }
+        char* line = malloc(sizeof(char) * len);
+        strcpy(line, input);
+        line[len] = '\0';
+        char* command = strtok(input, " ");
+
+        if (command == NULL) {
+            printf("Empty\n");
+        } else {
+            char* args = skip_string(line, len, strlen(command));
+            int args_count = 0;
+            char** arguments = split_string(args, ' ', &args_count);
+
+            if (strcmp(command, "exit") == 0 || strcmp(command, "quit") == 0 || strcmp(command, "q") == 0) {
+                flag = 0;
+            } else if (strcmp(command, "help") == 0 || strcmp(command, "h") == 0) {
+                help();
+            } else {
+                // Parsing commands
+                if (strcmp(command, "createfile") == 0) {
+                    // support --file <value>
+                    if (args_count == 0) {
+                        printf("createfile: invalid arguments try `--file <name>`\n");
+                    } else {
+                        char* name = NULL;
+                        for (int i = 0; i < args_count; i++) {
+                            if (strcmp(arguments[i], "--file") == 0) {
+                                if (i + 1 >= args_count) {
+                                    break;
+                                }
+                                name = arguments[i + 1];
+                                break;
+                            }
+                        }
+
+                        if (name == NULL) {
+                            printf("createfile: invalid arguments try `--file <name>`\n");
+                        } else createfile(name);
+                    }
+                }
+                printf("Checking %s.%s\n", command, args);
+                // parse(2, (char*[]){command, args});
+            }
+        }
     }
 
     return 0;
